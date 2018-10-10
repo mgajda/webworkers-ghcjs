d6317a5af1bf709785edc53e2286a681d1188dec
Balaji Rao <balajirrao@gmail.com>
2018-10-11 01:41:41 +0200

    Setup install handler
    There's a warning in the browser
    
    "Event handler of 'install' event must be added on the initial
    evaluation of worker script."

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Data.JSString (JSString)
+import GHCJS.Types
+import GHCJS.DOM.EventTarget
+import GHCJS.DOM.Event
+import GHCJS.DOM.EventTargetClosures
+
+foreign import javascript unsafe "self || $1" self ::  Int -> EventTarget
+foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
+foreign import javascript unsafe
+  "console.log($1)" console_log :: JSString -> IO ()
+
+installHandler :: Event -> IO ()
+installHandler e = console_log "Installed!"
 
 main :: IO ()
-main = someFunc
+main = do
+  l <- eventListenerNew installHandler
+  addEventListener (self 0) ("install" :: String) (Just l) False

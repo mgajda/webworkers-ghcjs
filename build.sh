837d23bf469d877e1bdd56179f22c5355dd5d388
Balaji Rao <balajirrao@gmail.com>
2018-10-17 22:35:13 +0200

    Make build idempotent

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 # Copy over the javascript
 stack build
-rm -f server/static/all.js
 cp $(stack path --local-install-root)/bin/webworker-ghcjs.jsexe/all.js html
 cp $(stack path --local-install-root)/bin/webworker-ghcjs.jsexe/all.js.externs html
 
 # Remove the last line which runs main
-sed -i 's/h$main(h$mainZCZCMainzimain)//g' html/all.js
+sed 's/h$main(h$mainZCZCMainzimain)//g' html/all.js > html/all.dontrunmain.js
 
 # cat the fetch install handler
-cat html/install_fetch_handler.js >> html/all.js
+cat html/all.dontrunmain.js html/install_fetch_handler.js > html/sw.js

582b31791b4460659c325e81b804fd98482907ea
Balaji Rao <balajirrao@gmail.com>
2018-10-17 08:03:26 +0200

    WIP
    * app/Main.hs:

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,10 @@ import GHCJS.DOM.Types (JSM, Request, toJSVal)
 import GHCJS.DOM.EventTarget
 import GHCJS.DOM.Event
 import GHCJS.DOM.EventTargetClosures
-import GHCJS.DOM.Response
+import GHCJS.DOM.Response (Response)
 import GHCJS.DOM.WorkerGlobalScope (WorkerGlobalScope)
+import GHCJS.DOM.Request
+import GHCJS.DOM.Headers
 
 foreign import javascript safe "$r = self" getSelf :: IO WorkerGlobalScope
 foreign import javascript unsafe "window.alert($1)" js_alert :: JSString -> IO ()
@@ -18,19 +20,27 @@ foreign import javascript unsafe
 
 newtype Promise = Promise JSVal
 
+foreign import javascript safe "$r = $1[$2]" off :: JSVal -> JSVal -> JSVal
+foreign import javascript safe "$r = {172: { 20: {4: 'eu-server.migamake.com'}}}" serverMap :: JSVal
 foreign import javascript unsafe "$1.respondWith($2)" respondWith :: Event -> Promise -> IO ()
 foreign import javascript unsafe "$1.request" getRequest :: Event -> Request
 foreign import javascript unsafe "$1.fetch($2)" fetch :: WorkerGlobalScope -> Request -> IO Promise
 
 -- TODO: Request routing logic
-routeRequest :: Request -> Request
-routeRequest = id
+routeRequest :: Request -> IO Request
+routeRequest r = do
+  h <- getHeaders r
+  cfip <- (get h ("cf-connecting-ip" :: String)) :: IO (Maybe String)
+  case cfip of
+    Nothing -> return r
+    Just t  -> 
 
 fetchHandler :: Event -> IO()
 fetchHandler e = do
   self <- getSelf
-  console_log "Fetching.."
-  resp <- (fetch self (routeRequest $ getRequest e))
+  console_log "Routing.."
+  r <- routeRequest $ getRequest e
+  resp <- fetch self r
   respondWith e resp
 
 main :: IO ()
